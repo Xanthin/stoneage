23bbea93263d7191745b952da9416a6b4f867bc4
Xanthin <Xanthin@users.noreply.github.com>
2014-04-09 00:30:16 +0200

    back to old overwrite with full moretrees support

--- overwrite.lua
+++ overwrite.lua
@@ -1,47 +1,108 @@
 --
--- overwrite nodes and tools
+-- overwrite default nodes and tools
 --
 
-minetest.override_item("default:axe_wood", {
-	tool_capabilities = {
-		full_punch_interval = 1.0,
-		max_drop_level=0,
-		groupcaps={
-		}
-	}
-})
-	
-minetest.override_item("default:pick_wood", {
-	tool_capabilities = {
-		full_punch_interval = 1.0,
-		max_drop_level=0,
-		groupcaps={
-		}
-	}
-})
+-- stoneage = {}
+local entity
 
-local trees = {
-	"default:tree",
-	"default:jungletree",
-	"moretrees:beech_trunk",
-	"moretrees:apple_tree_trunk",
-	"moretrees:oak_trunk",
-	"moretrees:sequoia_trunk",
-	"moretrees:birch_trunk",
-	"moretrees:palm_trunk",
-	"moretrees:spruce_trunk",
-	"moretrees:pine_trunk",
-	"moretrees:willow_trunk",
-	"moretrees:rubber_tree_trunk",
-	"moretrees:jungletree_trunk",
-	"moretrees:fir_trunk",
-	"conifers:trunk",
-	"conifers:trunk_reversed",
-}
-
-for _,name in ipairs(trees) do
-	if minetest.registered_items[name] then
-		groups = {tree=1,choppy=2,flammable=2}
-		minetest.override_item(name, {groups = groups})
+-- registered
+local registered = function(case,name)
+	local params = {}
+	local list
+	if case == "item" then list = minetest.registered_items end
+	if case == "node" then list = minetest.registered_nodes end
+	if case == "craftitem" then list = minetest.registered_craftitems end
+	if case == "tool" then list = minetest.registered_tools end
+	if case == "entity" then list = minetest.registered_entities end
+	if list then
+		for k,v in pairs(list[name]) do
+			params[k] = v
+		end
 	end
+	return params
 end
+
+-- tree
+entity = registered("node","default:tree")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":default:tree", entity)
+
+-- beech_tree
+entity = registered("node","moretrees:beech_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:beech_trunk", entity)
+
+-- apple_tree
+entity = registered("node","moretrees:apple_tree_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:apple_tree_trunk", entity)
+
+-- oak_tree
+entity = registered("node","moretrees:oak_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:oak_trunk", entity)
+
+-- sequoia_tree
+entity = registered("node","moretrees:sequoia_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:sequoia_trunk", entity)
+
+-- birch_tree
+entity = registered("node","moretrees:birch_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:birch_trunk", entity)
+
+-- palm_tree
+entity = registered("node","moretrees:palm_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:palm_trunk", entity)
+
+-- spruce_tree
+entity = registered("node","moretrees:spruce_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:spruce_trunk", entity)
+
+-- moretrees:pine_trunk
+entity = registered("node","moretrees:pine_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:pine_trunk", entity)
+
+-- moretrees:rubber_tree_trunk
+entity = registered("node","moretrees:rubber_tree_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:rubber_tree_trunk", entity)
+
+-- moretrees:willow_trunk
+entity = registered("node","moretrees:willow_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:willow_trunk", entity)
+
+-- moretrees:fir_trunk
+entity = registered("node","moretrees:fir_trunk")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":moretrees:fir_trunk", entity)
+
+-- jungletree
+entity = registered("node","default:jungletree")
+entity.groups = {tree=1,choppy=2,flammable=2}
+minetest.register_node(":default:jungletree", entity)
+
+-- wooden axe
+entity = registered("tool","default:axe_wood")
+entity.tool_capabilities = {
+			max_drop_level=0,
+			groupcaps={
+				choppy={times={[3]=0.80}, uses=10, maxlevel=1},
+				fleshy={times={[2]=1.50, [3]=0.80}, uses=10, maxlevel=1},
+			}
+		}
+minetest.register_tool(":default:axe_wood", entity)
+
+-- wooden pickse
+entity = registered("tool","default:pick_wood")
+entity.tool_capabilities = {
+			max_drop_level=0,
+			groupcaps={
+			}
+		}
+minetest.register_tool(":default:pick_wood", entity)
